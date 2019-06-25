15c30a9c9497dd2f00d6bffe75b67aa3d98854a5
Trenton L. Taylor <ttaylor@tron.thegrid.trenton.io>
2019-06-24 22:22:16 -0500

    Finished code for resume dates, cleaned up admin panel for resume positions, set secret key to django dir

--- django_personal_website/settings.py
+++ django_personal_website/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('/srv/django_personal_website/secret.key') as f:
+with open('secret.key') as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['trenton.io', 'www.trenton.io', 'test.trenton.io']
+ALLOWED_HOSTS = ['trenton.io', 'www.trenton.io', 'test.trenton.io', '127.0.0.1']
 
 
 # Application definition
@@ -137,15 +137,15 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Security
-SECURE_HSTS_SECONDS = 60
-SECURE_HSTS_INCLUDE_SUBDOMAINS = False ## Update later
-SECURE_HSTS_PRELOAD = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_BROWSER_XSS_FILTER = True
-X_FRAME_OPTIONS = 'DENY'
+#SECURE_HSTS_SECONDS = 60
+#SECURE_HSTS_INCLUDE_SUBDOMAINS = False ## Update later
+#SECURE_HSTS_PRELOAD = True
+#SECURE_CONTENT_TYPE_NOSNIFF = True
+#SECURE_SSL_REDIRECT = True
+#SESSION_COOKIE_SECURE = True
+#CSRF_COOKIE_SECURE = True
+#SECURE_BROWSER_XSS_FILTER = True
+#X_FRAME_OPTIONS = 'DENY'
 
 # Email
 DEFAULT_FROM_EMAIL = 'noreply@www.trenton.io'
--- resume/admin.py
+++ resume/admin.py
@@ -2,9 +2,18 @@ from django.contrib import admin
 from resume.models import  Employer, Position, BulletPoint, Skill, Education, Award
 # Register your models here.
 
+
+
+class BulletPointInline(admin.TabularInline):
+    model = BulletPoint
+
 admin.site.register(Employer)
-admin.site.register(Position)
-admin.site.register(BulletPoint)
 admin.site.register(Skill)
 admin.site.register(Education)
 admin.site.register(Award)
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_filter = ('hide',)
+    
+    inlines = [BulletPointInline]
