008ad3f27feaaef92edaaa03558ff3970c30eda8
Trenton Taylor <trentonlloydtaylor@gmail.com>
2019-08-12 14:17:37 -0500

    Fix sort by end_date

--- resume/views.py
+++ resume/views.py
@@ -1,6 +1,7 @@
 """Defines how pages will get rendered"""
 from django.shortcuts import render
 from resume.models import  Employer, Skill, Education, Award, Bio
+from django.db.models import Max
 
 # Create your views here.
 
@@ -13,7 +14,7 @@ def resume(request):
     except Bio.DoesNotExist:
         bio = None
 
-    employers = Employer.objects.filter(hide=False)
+    employers = Employer.objects.filter(hide=False).annotate(ed=Max('position__end_date')).order_by('ed')
     skills = Skill.objects.filter(hide=False)
     education = Education.objects.filter(hide=False)
     awards = Award.objects.filter(hide=False)
