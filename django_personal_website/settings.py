621da1af73f40f3561d32010009d5e5c78224853
Trenton L. Taylor <ttaylor@tron.thegrid.trenton.io>
2019-11-02 12:29:01 -0500

    More static tweaks - readded STATICFILES_DIRS

--- django_personal_website/settings.py
+++ django_personal_website/settings.py
@@ -164,6 +164,11 @@ SRV_DIR = '/srv/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATICFILES_DIRS = (
+    'static',
+    os.path.join(BASE_DIR, 'static'),
+)
+
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(SRV_DIR, 'static')
 
