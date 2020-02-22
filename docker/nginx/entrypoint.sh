2f0b1754430287a141ddb8ab5674d1f03f80410d
Trenton Taylor <trentonlloydtaylor@gmail.com>
2020-02-21 20:44:59 -0600

    exec stuff

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 set -eu
 
+echo 'Running substitutions for nginx'
 envsubst '${NGINX_SERVER_NAME}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
