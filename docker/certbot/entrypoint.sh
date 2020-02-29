2f4efa03f061657ab37e535780795c550798f52b
Trenton Taylor <trentonlloydtaylor@gmail.com>
2020-02-29 00:55:26 -0600

    Readded endning quote

--- docker/certbot/entrypoint.sh
+++ docker/certbot/entrypoint.sh
@@ -7,7 +7,7 @@ if [ ! -f "/etc/letsencrypt/live/$CERTBOT_SERVER_NAME" ]; then
     certbot certonly --non-interactive  --rsa-key-size 4096 --no-self-upgrade --dns-cloudflare --dns-cloudflare-propagation-seconds 60 --dns-cloudflare-credentials /cloudflare.key --email certs@trenton.io --agree-tos --server https://acme-staging-v02.api.letsencrypt.org/directory -d "$CERTBOT_DOMAIN_LIST" # https://acme-v02.api.letsencrypt.org/directory
 fi
 while :; do
-    certbot renew --non-interactive --no-self-upgrade --dns-cloudflare --dns-cloudflare-propagation-seconds 60 --dns-cloudflare-credentials /cloudflare.key --email certs@trenton.io --agree-tos --server https://acme-staging-v02.api.letsencrypt.org/directory -d "$CERTBOT_DOMAIN_LIST # https://acme-v02.api.letsencrypt.org/directory
+    certbot renew --non-interactive --no-self-upgrade --dns-cloudflare --dns-cloudflare-propagation-seconds 60 --dns-cloudflare-credentials /cloudflare.key --email certs@trenton.io --agree-tos --server https://acme-staging-v02.api.letsencrypt.org/directory -d "$CERTBOT_DOMAIN_LIST" # https://acme-v02.api.letsencrypt.org/directory
     sleep 12h & wait $${!}
 done
 git gc --prune=now
