74e5da02505eb60111f8427f6d6cd4a9770f3c7f
Trenton Taylor <trentonlloydtaylor@gmail.com>
2020-02-29 01:09:09 -0600

    tweaking while loop

--- docker/certbot/entrypoint.sh
+++ docker/certbot/entrypoint.sh
@@ -8,6 +8,6 @@ if [ ! -f "/etc/letsencrypt/live/$CERTBOT_SERVER_NAME" ]; then
 fi
 while :; do
     certbot renew --non-interactive --no-self-upgrade --dns-cloudflare --dns-cloudflare-propagation-seconds 60 --dns-cloudflare-credentials /cloudflare.key --email certs@trenton.io --agree-tos --server https://acme-staging-v02.api.letsencrypt.org/directory # https://acme-v02.api.letsencrypt.org/directory
-    sleep 12h & wait $${!}
+    sleep 12h & wait $!
 done
 git gc --prune=now
