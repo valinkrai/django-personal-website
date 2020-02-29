812ebe39505c63c7abac47d5544860432fb15d6f
Trenton Taylor <trentonlloydtaylor@gmail.com>
2020-02-28 23:54:12 -0600

    Moving things around

--- docker/certbot/entrypoint.sh
+++ docker/certbot/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 set -eu
 trap exit TERM
+echo $CERTBOT_API_KEY > /cloudflare.key
 
-if [ ! -f "$FILE" ]; then
-    certbot certonly --non-interactive  --rsa-key-size 4096 --no-self-upgrade --dns-cloudflare --dns-cloudflare-propagation-seconds 30 --dns-cloudflare-credentials /cloudflare.key --email certs@trenton.io --agree-tos --server https://acme-staging-v02.api.letsencrypt.org/directory # https://acme-v02.api.letsencrypt.org/directory
+if [ ! -f "/etc/letsencrypt/live/$CERTBOT_SERVER_NAME" ]; then
+    certbot certonly --non-interactive  --rsa-key-size 4096 --no-self-upgrade --dns-cloudflare --dns-cloudflare-propagation-seconds 30 --dns-cloudflare-credentials /cloudflare.key --email certs@trenton.io --agree-tos --server https://acme-staging-v02.api.letsencrypt.org/directory -d $CERTBOT_DOMAIN_LIST # https://acme-v02.api.letsencrypt.org/directory
 fi
 while :; do
-    certbot renew --non-interactive --no-self-upgrade --dns-cloudflare --dns-cloudflare-propagation-seconds 30 --dns-cloudflare-credentials /cloudflare.key --email certs@trenton.io --agree-tos --server https://acme-staging-v02.api.letsencrypt.org/directory # https://acme-v02.api.letsencrypt.org/directory
+    certbot renew --non-interactive --no-self-upgrade --dns-cloudflare --dns-cloudflare-propagation-seconds 30 --dns-cloudflare-credentials /cloudflare.key --email certs@trenton.io --agree-tos --server https://acme-staging-v02.api.letsencrypt.org/directory -d $CERTBOT_DOMAIN_LIST # https://acme-v02.api.letsencrypt.org/directory
     sleep 12h & wait $${!}
 done
