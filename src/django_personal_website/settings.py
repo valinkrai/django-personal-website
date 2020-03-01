f29dc38a98ab9ad05c88619350020354409cc425
Trenton L Taylor <trentonlloydtaylor@gmail.com>
2020-02-29 20:42:38 -0600

    Update settings.py

--- src/django_personal_website/settings.py
+++ src/django_personal_website/settings.py
@@ -35,8 +35,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = not PROD
 
-ALLOWED_HOSTS = ['trenton.io', 'www.trenton.io', 'bradley.thegrid.trenton.io','alan.thegrid.trenton.io','www.thegrid.trenton.io','test.trenton.io', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = os.environ["DJANGO_SECRET_KEY"].split(',')
 
 # Application definition
 
