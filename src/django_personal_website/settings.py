ad9c7170391755c09b872a95e79bc7be277dd1e8
Trenton L Taylor <trentonlloydtaylor@gmail.com>
2020-02-29 20:53:17 -0600

    Update settings.py

--- src/django_personal_website/settings.py
+++ src/django_personal_website/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = not PROD
 
-ALLOWED_HOSTS = os.environ["DJANGO_SECRET_KEY"].split(',')
+ALLOWED_HOSTS = os.environ["DJANGO_DOMAIN_LIST"].split(',')
 
 # Application definition
 
